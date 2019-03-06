3e000bcc968d28b16d636d0bc3c85ca04ab64a44
Zhu Yuncheng <chuwanshing@gmail.com>
2019-03-06 14:45:42 +0800

    :sparkles: climbing stairs

---
+++ src/main/java/top/zhuyuncheng/leetcode/climbing_stairs/Solution.java
@@ -0,0 +1,43 @@
+package top.zhuyuncheng.leetcode.climbing_stairs;
+
+/*
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * 注意：给定 n 是一个正整数。
+ *
+ * 示例 1：
+ * 输入： 2
+ * 输出： 2
+ * 解释： 有两种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶
+ * 2.  2 阶
+ * 示例 2：
+ * 输入： 3
+ * 输出： 3
+ * 解释： 有三种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶 + 1 阶
+ * 2.  1 阶 + 2 阶
+ * 3.  2 阶 + 1 阶
+ */
+class Solution {
+
+    /*
+     *  1 1 2 3 5 8 13
+     *  斐波那契数列的非递归形式
+     *  动态规划的思想，初始化dp矩阵，第 i 个元素(i >= 2)等于前两个元素的和 dp[i] = dp[i - 1] + dp[i - 2]
+     *  注意：非递归的无法通过
+     */
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int res = new Solution().climbStairs(6);
+        System.out.println(res);
+    }
+}
